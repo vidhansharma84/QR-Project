137e3dff8dd89f16b905bc51c44d32597d4ff0fa
vidhan <73869270+vidhansharma84@users.noreply.github.com>
2023-04-25 21:38:45 +0530

    dinner time

--- Admin/Settings.php
+++ Admin/Settings.php
@@ -15,7 +15,35 @@
 <div class="flex-1">
     <div class="p-10">
       <h1 class="text-3xl font-bold text-gray-800 uppercase">My Settings</h1>
-      <p class="mt-4 text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <div class="bg-white rounded-lg shadow-lg p-8">
+  <h2 class="text-lg font-medium mb-6">Settings</h2>
+  <form>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div>
+        <label for="name" class="block text-sm font-medium text-gray-700 mb-2">Name</label>
+        <input type="text" id="name" name="name" class="border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2 rounded-md" placeholder="Enter your name">
+      </div>
+      <div>
+        <label for="email" class="block text-sm font-medium text-gray-700 mb-2">Email</label>
+        <input type="email" id="email" name="email" class="border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2 rounded-md" placeholder="Enter your email address">
+      </div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+      <div>
+        <label for="password" class="block text-sm font-medium text-gray-700 mb-2">Password</label>
+        <input type="password" id="password" name="password" class="border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2 rounded-md" placeholder="Enter a new password">
+      </div>
+      <div>
+        <label for="confirm-password" class="block text-sm font-medium text-gray-700 mb-2">Confirm Password</label>
+        <input type="password" id="confirm-password" name="confirm-password" class="border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2 rounded-md" placeholder="Confirm your new password">
+      </div>
+    </div>
+    <div class="mt-6">
+      <button type="submit" class="bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600">Save Changes</button>
+    </div>
+  </form>
+</div>
+
     </div>
   </div>
 
