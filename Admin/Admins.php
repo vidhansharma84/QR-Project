45a23b3e2cbb61b805daf6bf3d5598cb478141a0
vidhan <73869270+vidhansharma84@users.noreply.github.com>
2023-04-25 21:13:03 +0530

    12

--- Admin/Admins.php
+++ Admin/Admins.php
@@ -15,7 +15,111 @@
 <div class="flex-1">
     <div class="p-10">
       <h1 class="text-3xl font-bold text-gray-800 uppercase">My Admin</h1>
-      <p class="mt-4 text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      
+      <div class="flex flex-col">
+  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <table class="min-w-full divide-y divide-gray-200">
+          <thead class="bg-gray-50">
+            <tr>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Name
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Email
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Role
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Status
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y divide-gray-200">
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-10 w-10 rounded-full" src="https://randomuser.me/api/portraits/women/17.jpg" alt="">
+                  </div>
+                  <div class="ml-4">
+                    <div class="text-sm font-medium text-gray-900">
+                      Jane Doe
+                    </div>
+                    <div class="text-sm text-gray-500">
+                      janedoe@example.com
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-sm text-gray-900">janedoe@example.com</div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                  Admin
+                </span>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                  Active
+                </span>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-4">View</a>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-4">Edit</a>
+                <a href="#" class="text-red-600 hover:text-red-900">Delete</a>
+              </td>
+            </tr>
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div 
+                class="flex items-center">
+<div class="flex-shrink-0 h-10 w-10">
+<img class="h-10 w-10 rounded-full" src="https://randomuser.me/api/portraits/men/5.jpg" alt="">
+</div>
+<div class="ml-4">
+<div class="text-sm font-medium text-gray-900">
+John Doe
+</div>
+<div class="text-sm text-gray-500">
+johndoe@example.com
+</div>
+</div>
+</div>
+</td>
+<td class="px-6 py-4 whitespace-nowrap">
+<div class="text-sm text-gray-900">johndoe@example.com</div>
+</td>
+<td class="px-6 py-4 whitespace-nowrap">
+<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+Admin
+</span>
+</td>
+<td class="px-6 py-4 whitespace-nowrap">
+<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+Inactive
+</span>
+</td>
+<td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+<a href="#" class="text-indigo-600 hover:text-indigo-900 mr-4">View</a>
+<a href="#" class="text-indigo-600 hover:text-indigo-900 mr-4">Edit</a>
+<a href="#" class="text-red-600 hover:text-red-900">Delete</a>
+</td>
+</tr>
+<!-- Add more rows as needed -->
+</tbody>
+</table>
+</div>
+</div>
+
+  </div>
+</div>
     </div>
   </div>
 
