3fa97194f186770d9d2a7ea6d5ec3507fe12b94c
vidhan <73869270+vidhansharma84@users.noreply.github.com>
2023-04-27 13:43:02 +0530

    Update index.php

--- Site/index.php
+++ Site/index.php
@@ -1,5 +1,6 @@
 <?php 
-
+ session_start();
+ include('../Backend/config.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,7 +26,7 @@
 		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 			<!-- Event card example -->
             <?php 
-      $sql="SELECT * from event where UID='$id'";
+      $sql="SELECT * from event ";
       $result=mysqli_query($conn,$sql);
       $present=mysqli_num_rows($result);
       if($present>0)
@@ -36,15 +37,15 @@
       
       ?>
 			<div class="bg-white rounded-lg shadow-md overflow-hidden">
-				<div class="bg-gray-200 h-40 md:h-48 w-full flex items-center justify-center">
-					<span class="text-2xl font-bold text-gray-700">June 27</span>
+				<div class="bg-gray-200 h-64 md:h-64 w-full flex items-center justify-center">
+					<img src="<?php echo $row['Pic'];?>" class="img-responsive w-full h-64"/>>
 				</div>
 				<div class="p-4">
 					<h3 class="text-lg font-bold mb-2"><?php echo $row['Title'];?></h3>
 					<p class="text-gray-700 mb-4"><?php echo $row['About'];?></p>
                     <p class="text-gray-700 mb-4">Location:-<?php echo $row['Location'];?></p>
-                    <p class="text-gray-700 mb-4">Amount:-<?php echo $row['Amount'];?></p>
-					<a href="#" class="inline-block bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600 transition-colors duration-300"><form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_Lik7b0kU6SGcZY" async> </script> </form></a>
+                    <p class="text-gray-700 mb-4">Amount:- ₹<?php echo $row['Amount'];?></p>
+					<a href="#" class="inline-block bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600 transition-colors duration-300"></a>
 				</div>
 			</div>
 
